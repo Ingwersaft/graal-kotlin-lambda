ace24748e0d51ecb012aa9d6a11cbca8413673b6
Ingwersaft <Ingwersaft@users.noreply.github.com>
2019-03-26 21:33:16 +0100

    added gradle wrapper +x flag; fixed openjdk install; added gcc and headers

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ also used: gradle + terraform
 ## status
 - [x] basic gradle setup
 - [x] terraform ec2 (for build, graalvm only works on *nix)
-- [ ] graalvm task works
+- [x] graalvm task works
 - [ ] create http client for lambda runtime interface
 - [ ] terraform lambda using graalvm image
 - [ ] replace manual ec2 build with codebuild from git repo and automatic lambda rollout
--- init_instance.sh
+++ init_instance.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 yum -y update
-yum -y install git java-1.8.0-openjdk-devel gcc gcc-c++
+yum -y install git java-1.8.0-openjdk-devel gcc gcc-c++ glibc-devel zlib-devel zlib
 git clone https://github.com/Ingwersaft/graal-kotlin-lambda.git /home/ec2-user/graal-kotlin-lambda
 chown ec2-user:ec2-user /home/ec2-user/graal-kotlin-lambda
 chmod +x /home/ec2-user/graal-kotlin-lambda/gradlew
