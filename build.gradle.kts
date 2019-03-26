9eb9ec556a4e99e2714cb75b9155d553ae45833b
Ingwersaft <Ingwersaft@users.noreply.github.com>
2019-03-26 20:07:27 +0100

    added basic gradle setup

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.3.21"
+    id("com.palantir.graal") version "0.3.0-7-gdb167e6"
+}
+
+group = "com.mkring.graalkotlinlambda"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+graal {
+    graalVersion("1.0.0-rc14")
+    mainClass("com.mkring.graalkotlinlambda.MainKt")
+    outputName("graal-kotlin-lambda-kt")
+    option("--enable-http")
+}
---
+++ src/main/kotlin/com/mkring/graalkotlinlambda/Main.kt
@@ -0,0 +1,5 @@
+package com.mkring.graalkotlinlambda
+
+fun main() {
+    println("hello world")
+}
