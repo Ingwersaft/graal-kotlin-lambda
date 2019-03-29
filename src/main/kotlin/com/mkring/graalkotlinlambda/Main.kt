3a77dd405be6478b6b7508eb6e6023df0058cd21
Ingwersaft <Ingwersaft@users.noreply.github.com>
2019-03-29 18:20:57 +0100

    added very basic lambda consume loop

--- src/main/kotlin/com/mkring/graalkotlinlambda/Main.kt
+++ src/main/kotlin/com/mkring/graalkotlinlambda/Main.kt
@@ -3,23 +3,41 @@ package com.mkring.graalkotlinlambda
 import com.google.gson.GsonBuilder
 import org.apache.http.HttpEntity
 import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
 
 val gson = GsonBuilder().setPrettyPrinting().serializeNulls().create()
 fun main() {
     println("hello world")
+    val lambdaRuntimeApi = System.getenv("AWS_LAMBDA_RUNTIME_API")
     val httpClient = HttpClients.createDefault()
-    httpClient.execute(HttpGet("http://jsonplaceholder.typicode.com/todos/1")).let {
-        println(it.statusLine)
-        val stringPayload = it.entity.s()
-        println(stringPayload)
-        println(gson.fromJson<Todo>(stringPayload, Todo::class.java))
-        EntityUtils.consume(it.entity)
-        it.close()
+    while (true) {
+        httpClient.execute(HttpGet("http://$lambdaRuntimeApi/2018-06-01/runtime/invocation/next")).also {
+            println("it.statusLine=${it.statusLine}")
+            val stringPayload = it.entity.s()
+            println("stringPayload=$stringPayload")
+
+//            println(gson.fromJson<Todo>(stringPayload, Todo::class.java))
+
+            val awsRequestId = it.getHeaders("Lambda-Runtime-Aws-Request-Id").first()
+            println("awsRequestId=$awsRequestId")
+            EntityUtils.consume(it.entity)
+            it.close()
+
+            httpClient.execute(HttpPost("http://$lambdaRuntimeApi/2018-06-01/runtime/invocation/$awsRequestId/response").also {
+                it.entity = StringEntity("SUCCESS")
+            }).also {
+                println("it.statusLine=${it.statusLine}")
+                EntityUtils.consume(it.entity)
+                it.close()
+            }
+        }
+        println("done")
     }
-    println("done")
 }
+
 
 private fun HttpEntity.s(): String? = EntityUtils.toString(this)
 
